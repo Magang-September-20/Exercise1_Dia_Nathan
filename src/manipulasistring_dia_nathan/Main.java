0a5cc3625cf2b0f9fe78e61d25890c516239da9d
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2020-09-14 03:08:07 +0700

    fix username anti bug
    sudah teruji

--- src/manipulasistring_dia_nathan/Main.java
+++ src/manipulasistring_dia_nathan/Main.java
@@ -27,10 +27,6 @@ public class Main {
     public Main() {
         tampung = new LinkedList<>();
     }
-
-    public void isiData(String namaLengkap, String password) {
-
-//      manipulasi string disini
 /**
  * Ini merupakan fungsi untuk manipulasi string.
  * variable nama lengkap akan dipecah menjadi array yang dipisahkan dengan spasi.
@@ -38,6 +34,9 @@ public class Main {
  * jika panjang array split memiliki panjang lebih dari satu maka lastName didapat dari index array terakhir.
  * lalu firstName didapat dari index array awal ditambah dengan index array berikutnya kemudian digabung dan diberi spasi.
  */
+    public void isiData(String namaLengkap, String password) {
+        int id = ID.incrementAndGet();
+//        manipulasi string disini
         String lastName="",firstName="", username="";
         String[] split = namaLengkap.split(" ");
         if (split.length <=1){
@@ -51,15 +50,26 @@ public class Main {
         }
         
 //      jika index split > 2 maka username otomatis mengambil index pertama dan 2 huruf lastname
-        username = split[0] + lastName.substring(0, 2);
+        try {
+            username = split[0] + lastName.substring(0, 2);
+        } catch (StringIndexOutOfBoundsException e) {
+            username = split[0] + split[split.length-1];
+//            System.out.println("Bentuk Nama Lengkap Salah! (tiap kata tidak boleh hanya 1 huruf!)");
+        }
 //        ngecek apakah username sudah ada di dalam employee
-        for (int i = 0; i < tampung.size(); i++) {
-                if(tampung.get(i).getUsername().equals(username)){
-                    username = split[0] + split[split.length-i-2].substring(0,2);
-//                    System.out.println("username sudah ada");
+        if(isUsernameTaken(username)){
+            for (int i = 2; i <= split.length; i++) {
+                username = split[0] + split[split.length-i].substring(0,2);
+                if(!isUsernameTaken(username))
+                {
+                    break;
                 }
             }
-        tampung.add(new Employees(firstName, lastName, username, password, ID.incrementAndGet()));
+            if(isUsernameTaken(username)){
+                username = username + id;
+            }
+        }
+        tampung.add(new Employees(firstName, lastName, username, password, id));
         System.out.println("--Data ditambah--");
 
     }
@@ -108,18 +118,20 @@ public class Main {
  * jika username dan password di dalam list sesuai dengan scanUsername dan scanPass yg kita isikan maka akan berhasil, jika berbeda makan akan gagal
  * dan jika username dan password tidak sama maka akan mencetak data tidak ditemukan.
  */
-            
-            for (int i = 0; i < tampung.size(); i++) {
-                if(tampung.get(i).getUsername().equals(scanUsername)){
-                    boolean cekPass = BCrypt.checkpw(scanPass, tampung.get(i).getPassword());
-                    if(cekPass){
-                        System.out.println("LOGIN BERHASIL !!");
-                    } else {
-                        System.out.println("LOGIN GAGAL !!");
-                    } 
-                }else{
-                    System.out.println("DATA TIDAK DITEMUKAN!!!");
+            if(isUsernameTaken(scanUsername)){
+                for (int i = 0; i < tampung.size(); i++) {
+                    if(tampung.get(i).getUsername().equals(scanUsername)){
+                        boolean cekPass = BCrypt.checkpw(scanPass, tampung.get(i).getPassword());
+                        if(cekPass){
+                            System.out.println("LOGIN BERHASIL !!");
+                        } else {
+                            System.out.println("LOGIN GAGAL !!");
+                        }
+                    }
                 }
+            }
+            else{
+                System.out.println("USERNAME NYA GA ADA!!!!");
             }
         }
     }
@@ -148,25 +160,37 @@ public class Main {
                 continue;
             }
 
-            if (pilih == 1) {
-                System.out.println("=================");
-                System.out.println("Add");
-                System.out.println("=================");
-                System.out.print("Nama Lengkap   : ");
-                namaLengkap = scan.next();
-                System.out.print("Password       : ");
-
-                pass = scan.next();
-
-                tampildata.isiData(namaLengkap, pass);
-            } else if (pilih == 2) {
-                tampildata.showData();
-            }
-            else if(pilih == 3){
-                tampildata.login();
+            switch (pilih) {
+                case 1:
+                    System.out.println("=================");
+                    System.out.println("Add");
+                    System.out.println("=================");
+                    System.out.print("Nama Lengkap   : ");
+                    namaLengkap = scan.next();
+                    System.out.print("Password       : ");
+                    pass = scan.next();
+                    tampildata.isiData(namaLengkap, pass);
+                    break;
+                case 2:
+                    tampildata.showData();
+                    break;
+                case 3:
+                    tampildata.login();
+                    break;
+                default:
+                    break;
             }
         } while (pilih != 4);
         System.out.println("Program Selesai");
+    }
+
+    private boolean isUsernameTaken(String username) {
+        for (Employees employees : tampung) {
+            if(employees.getUsername().equals(username)){
+                return true;
+            }
+        }
+        return false;
     }
     
     
